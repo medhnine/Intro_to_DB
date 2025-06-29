2b20394834555e45c309dc8c7db56d18e0ecc13e
Mohamed HNINE <43042061+medhnine@users.noreply.github.com>
2025-06-29 13:48:58 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,8 +17,10 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
             
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error connecting to MySQL: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
         
     finally:
         if cursor:
