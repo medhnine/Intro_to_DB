a3b59f274c90a71655caf03ce8ad05cf43008777
Mohamed HNINE <43042061+medhnine@users.noreply.github.com>
2025-06-29 13:59:02 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,14 @@
-USE alx_book_store;
-SHOW COLUMNS FROM books;
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books'
+ORDER BY 
+    ORDINAL_POSITION;
