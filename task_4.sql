d3ea79f0344f2d2f4b20fc423737296fd2316ddf
Mohamed HNINE <43042061+medhnine@users.noreply.github.com>
2025-06-29 14:03:00 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,3 @@
-USE alx_book_store;
 SELECT 
     COLUMN_NAME AS 'Field',
     COLUMN_TYPE AS 'Type',
@@ -10,6 +9,6 @@ FROM
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books'
+    AND TABLE_NAME = 'Books'
 ORDER BY 
     ORDINAL_POSITION;
